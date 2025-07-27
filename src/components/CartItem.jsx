d14907e20fb2f9d040dd21bf64acf63f517f44d2
atiya-inayat <atiyainayat869@gmail.com>
2025-07-27 19:59:02 +0500

    creating store

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartItem = () => {
+  return (
+    <>
+      <h1>individual item inside the cart.</h1>
+    </>
+  );
+};
+
+export default CartItem;
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductList = () => {
+  return <div>to show products and add them to cart.</div>;
+};
+
+export default ProductList;
